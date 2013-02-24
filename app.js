5b65d878d169228445617ce8809501b9de521584
Ted Jenkins <buffalobillion@gmail.com>
2013-02-24 16:47:00 -0700

    Removed jade

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.delete('/api/post/:id', function(req, res) {
 });
 
 // Start server
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
